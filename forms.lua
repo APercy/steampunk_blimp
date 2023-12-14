f23a60d1efc1f290201110d7e1890fedf8b923c1
Alexsandro Percy <alexsandro.percy@gmail.com>
2023-12-14 20:43:50 -0300

    added xmas logo and optimized the textures

--- forms.lua
+++ forms.lua
@@ -64,7 +64,7 @@ function steampunk_blimp.logo_formspec(name)
         "size[6,4]",
 	}, "")
 
-    local logos = {"blimp_clover.png","blimp_liz.png","blimp_shotting_star.png","blimp_skull.png", "blimp_jack.png",}
+    local logos = {"blimp_clover.png","blimp_liz.png","blimp_shotting_star.png","blimp_skull.png", "blimp_jack.png", "blimp_xmas.png"}
     local logolist = ""
     for k, v in pairs(logos) do
         logolist = logolist .. v .. ","
