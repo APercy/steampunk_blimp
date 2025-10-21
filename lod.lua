d29fbc30ea8e0c66dd8fa74a00cafcd9aa0ca30b
Fábio Rodrigues Ribeiro <farribeiro@gmail.com>
2025-10-21 12:58:25 -0300

    Change to core namespace lod

--- lod.lua
+++ lod.lua
@@ -1,7 +1,7 @@
 
 local function is_pos_in_range(pos, range)
 	if not pos then return false end
-    local connected_players = minetest.get_connected_players()
+    local connected_players = core.get_connected_players()
 	for _, player in pairs(connected_players) do
 		if vector.distance(player:get_pos(),pos)<=range then return true end
 	end
